58d3810de40449d8fd64ce1992bae9774016bc62
venkteshmane <venktesh.mane18@gmail.com>
2022-06-16 20:48:23 +0530

    Employee Payroll home page created, created Add Employee Button, Employee Details in Tabular Format, View Details from Local Storage.

---
+++ css/js/SitePropertyScript.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page: "C:\\Users\\user\\Desktop\\LFP_Batch\\Day45_Employee_Payroll_App\\css\js\\html\\Home.html",
+    add_emp_payroll_page: "C:\\Users\\user\\Desktop\\LFP_Batch\\Day45_Employee_Payroll_App\\css\js\\html"
+};
